893f937b907bb1fce6012e891506fe4439a02513
claugia13 <ac.delima13@gmail.com>
2020-12-17 15:15:35 -0700

    Add tarot and affirmations

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-// Mixed Messages project
+/*** Mixed Messages Codecademy Javascript Project 
+
+Theme: Tarot, numerology, and positive affirmations
+
+Step 1 - start gathering content for randomly generated messages ***/
+
+const tarot = ['The Fool', 'The Magician', 'The High Priestess', 'The Empress', 'The Emperor', 
+'The Hierophant', 'The Lovers', 'The Chariot', 'Strength', 'The Hermit', 'Wheel of Fortune', 
+'Justice', 'The Hanged Man', 'Death', 'Temperance', 'The Devil', 'The Tower', 'The Star', 
+'The Moon', 'The Sun', 'Judgement', 'The World'];
+
+// create tarot object that contains card names and card meanings
+
+
+// affirmations collected from https://www.developgoodhabits.com/positive-affirmations/
+const affirmations = ['My strength is greather than any struggle.', ]
+'I feel profound empathy and love for others and their own unique paths.',
+'My life is filled with love, joy and abundance.', 'My body is beautiful and expresses my spirit.',
+'I respect my limitations and thank myself for the things I am able to accomplish.',
+'I have all the tools I need to succeed.', 'I am empowered to create change in my life.',
+'I release negative thoughts and attachments, embracing positivity, gratitude, and love.',
+'I am kind and compassionate. I do not judge myself or others', 'I am enough.', 
+'I am right where I need to be.', 'I am a magnet for love and abundance.',
+'I am surrounded by love.', 'I am healthy, I am whole, I am healed, I am happy, I am free.', 
+'I will care for myself as much as I care for others.', 
+'I respect my limitations and thank myself for the things I am able to accomplish', 
+'I believe in my skills and abilities.', 'I am worthy of all the good things that happen in my life.',
+'I am in the flow of my destiny.', 'I achieve all of my goals.', 
+'I am calm, patient, and in control of my emotions.', 'New opportunities come easily to me.',
+'I have all the power I need to create the success I desire.', 
+'Everyting in my life is working out for my highest good.', 
+'My heart is filled with gratitude for all that I have and al that is coming.',
+'I fully let go and trust in divine timing.', 
+
+
